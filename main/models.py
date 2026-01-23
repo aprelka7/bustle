10d50d90e60192aa8e965dbf7d066554bca990ad
yalub <yalubluchay42@yandex.ru>
2026-01-23 19:18:46 +0300

    urls, urls, admin

---
+++ main/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    main_image = models.ImageField(upload_to='dishes/main/')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+    
+class DishImage(models.Model):
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='dishes/extra')
+        
